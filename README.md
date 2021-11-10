8bcbd93c5dda0b8263a00237c4b06146538e3606
Maryana Moravska <m.moravska@probegin.com>
2021-11-10 16:50:04 +0200

    add cv

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# CV_Moravska
+# Resume
+Hello! My name is Maryana.
+This repository consists my resume with styles which I created.
+
+If you want to meet me better, please do not hesitate to contact [me](https://maryanamor.github.io/homepage/)!
---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+//
+// Scripts
+//
+
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+const rootElement = document.documentElement;
+
+function scrollToTop() {
+    rootElement.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+scrollToTopBtn.addEventListener("click", scrollToTop);
